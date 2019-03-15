e73e97cfe7f6d39ee84d8c9e3093d08b3c2546f4
Keith Saniga <sanigak@outlook.com>
2019-03-15 10:08:56 -0400

    created virtual env, basic drawing methods

--- PlottingWithoutMatplotlib/PlottingWithoutMatplotlib.py
+++ PlottingWithoutMatplotlib/PlottingWithoutMatplotlib.py
@@ -1,1 +1,53 @@
+import turtle
+import pandas
+
+
+drawer = turtle.Turtle()
+
+turtle.setworldcoordinates(0, 0, 1000, 1000)
+
+def drawRectangle(height, width):
+    turtle.pd()
+    turtle.seth(0)
+    turtle.begin_fill()
+
+    #bottom
+    turtle.forward(width)
+    turtle.left(90)
+
+    #right
+    turtle.forward(height)
+    turtle.left(90)
+
+    #top
+    turtle.forward(width)
+    turtle.left(90)
+
+    #left
+    turtle.forward(height)
+    turtle.left(90)
+
+    turtle.end_fill()
+
+
+def setCoordinates(x,y):
+    turtle.pu()
+    turtle.setx(x)
+    turtle.sety(y)
+
+
+def drawAxes():
+    setCoordinates(10,10)
+    turtle.pd()
+    turtle.forward(900)
+    setCoordinates(10,10)
+    turtle.pd()
+    turtle.left(90)
+    turtle.forward(900)
+
+#drawAxes()
+#setCoordinates(20,20)
+#drawRectangle(200,30)
+#turtle.done()
+
 
